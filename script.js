47169e8e016e678e6d295a7a2123da5f97c8c9f1
lucaderuiter <lucaderuiter@outlook.com>
2020-01-29 02:54:32 +0100

    versie 12

---
+++ script.js
@@ -0,0 +1,21 @@
+var button = document.getElementById("filterknop");
+var filteractive = false;
+
+var filtermenu = document.getElementById("filtersection");
+
+button.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(filteractive == false) {
+        filtermenu.style.visibility = "visible"
+        filteractive = true;
+    } else {
+        filtermenu.style.visibility = "hidden"
+        filteractive = false;
+    }
+    
+    
+    
+}
+
+
